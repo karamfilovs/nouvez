6fdd1c3aa98ef9f1d3ed7221f65caa3b49887eb2
Alex Karamfilov <karamfilovs@gmail.com>
2020-03-15 13:45:03 +0200

    Added few locators in PDP page

--- src/main/java/pages/ProductDetailsPage.java
+++ src/main/java/pages/ProductDetailsPage.java
@@ -10,20 +10,47 @@ public class ProductDetailsPage extends BasePage {
     @FindBy(how = How.ID, using = "product-addtocart-button")
     private WebElement addToCartButton;
 
+    @FindBy(how = How.XPATH, using = "//h1")
+    private WebElement header;
+
     @FindBy(how = How.XPATH, using = "//span[@class='price']")
     private WebElement productPrice;
+
+    @FindBy(how = How.XPATH, using = "//span[@class='product-designer']")
+    private WebElement productInfo;
+
+
+    @FindBy(how = How.XPATH, using = "//a[@data-action='add-to-wishlist']")
+    private WebElement addToWishListLink;
+
+    @FindBy(how = How.XPATH, using = "//div[@data-network='sharethis']")
+    private WebElement shareLink;
 
 
     public ProductDetailsPage(WebDriver driver) {
         super(driver);
     }
 
-    public void clickAddToCartButton(){
+    public void clickAddToCartButton() {
         LOGGER.info("Clicking Add to cart button");
         click(addToCartButton);
     }
 
-    public String getProductPrice(){
+    public void clickAddToWishList() {
+        LOGGER.info("Clicking Add to Wishlist");
+        click(addToWishListLink);
+    }
+
+    public void clickShareLink() {
+        LOGGER.info("Clicking Share link");
+        click(shareLink);
+    }
+
+    public String getProductInfo() {
+        return getText(productInfo);
+    }
+
+    public String getProductPrice() {
         return getText(productPrice);
     }
 }
