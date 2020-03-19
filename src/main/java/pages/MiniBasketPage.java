7e04e6a382beb31960a63e2b2ce940e7f648792f
Pragmatic <todorov_yavor@abv.bg>
2020-03-19 11:08:38 +0200

    Created a Sting in MiniBasketPage for test purposes

--- src/main/java/pages/MiniBasketPage.java
+++ src/main/java/pages/MiniBasketPage.java
@@ -3,6 +3,9 @@ package pages;
 import org.openqa.selenium.WebDriver;
 
 public class MiniBasketPage extends BasePage {
+
+    String test;
+
     public MiniBasketPage(WebDriver driver) {
         super(driver);
     }
